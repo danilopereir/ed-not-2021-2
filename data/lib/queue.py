bf2a064b19f43cd974b823edb1ac062765217057
danilopereir <pdanilo205@gmail.com>
2021-09-30 23:06:25 +0000

    ed-not-2021-2

---
+++ data/lib/queue.py
@@ -0,0 +1,11 @@
+class Queue:
+    """
+        ESTRUTURAS DE DADOS FILA
+        - Fila é uma lista linear de acesso restrito, que permite apenas as operações
+        de enfileiramento (enqueue) e desenfileiramento (dequeue), a primeira
+        ocorrendo no final da estrutura e a segunda no início da estrutura.
+        - Como consequência, a fila funciona pelo princípio FIFO (First In, First Out 
+        - primeiro a entrar, primeiro a sair).
+        - A principal aplicação das filas são tarefas que envolvem o processamento de
+        tarefas por ordem de chegada.
+    """
